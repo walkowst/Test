d0fdc0d83049a2c0bbaeedc90bd5783bcebe456f
walkowst <tomasz.walkowski@gmail.com>
2015-07-23 06:54:06 +0100

    First commit

---
+++ Test/src/com/tomek/main/TestMain.java
@@ -0,0 +1,13 @@
+package com.tomek.main;
+/**
+ * Sample Test Project
+ * @author Tomek
+ *
+ */
+public class TestMain {
+
+	public static void main(String[] args){
+		
+		System.out.println("Hello, GitHub!");
+	}
+}
